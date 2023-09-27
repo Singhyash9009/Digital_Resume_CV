32cbc81833ce33190577cac2789d5a093bc64956
Singhyash9009 <121798886+Singhyash9009@users.noreply.github.com>
2023-09-27 13:08:25 +0530

    modifications

--- res.py
+++ res.py
@@ -4,24 +4,27 @@ from PIL import Image
 
 current_dir=Path(__file__).parent if "__file__" in locals() else Path.cwd()
 css_file= current_dir/"styles"/"main.css"
-resume_file=current_dir/"assets"/"YASH S Resume.pdf"
+resume_file=current_dir/"assets"/"Yash Singh Resume.pdf"
 profile_pic=current_dir/"assets"/"IMG_20230926_200253-fotor-202309270811.png"
 GitHub=current_dir/"assets"/"GitHub.png"
 LinkedIn=current_dir/"assets"/"LinkedIn.png"
+Gmail=current_dir/"assets"/"Gmail.png"
+
 #-----general setting----#
 PAGE_TITLE ="Digital CV | Yash Singh"
 PAGE_ICON  =":wave:"
 NAME      ="Yash Singh"
 DESCRIPTION="""
-I'm skilled and certified Data Scientist with competence in statistical modelling,data mining and data extraction.Looking for a
-responsible full-time career opportunity in a reputable organization where I can work with experienced professionals and reach my full
+I am a skilled and certified Data Scientist proficient in statistical modeling, data mining, and data extraction. I am actively seeking a fulltime
+career opportunity in a reputable organization where I can collaborate with experienced professionals and realize my full
 potential.
 """
 
 EMAIL  ="singhyash9009@gmail.com"
 SOCIAL_MEDIA={
     'LinkedIn':"https://www.linkedin.com/in/yash-singh-02b7341a3/",
-    'GitHub'  :"https://github.com/Singhyash9009/GIT-HUB"
+    'GitHub'  :"https://github.com/Singhyash9009/GIT-HUB",
+    'Gmail'   :"sinyash9009@gmail.com"
 }
 
 PROJECTS ={
@@ -30,7 +33,7 @@ PROJECTS ={
     "🏆Classification_model_mini_project":"https://github.com/Singhyash9009/GIT-HUB/blob/main/MINI%20PROJECT/MINI%20PROJECT%20%202%20(CLASSIFICATION%20MODEL)%20YASH%20SINGH.ipynb",
     "🏆 Clustering_model_mini_project"    :"https://github.com/Singhyash9009/GIT-HUB/blob/main/MINI%20PROJECT/MINI%20PROJECT%203%20(CLUSTERING%20MODEL)%20YASH%20SINGH.ipynb",
     "🏆 Summary_app_tool":"https://github.com/Singhyash9009/GIT-HUB/tree/main/Summary%20App%20Tool",
-    "🏆 Resume Builder with StreamLit":"https://github.com/Singhyash9009/GIT-HUB/tree/main/Resume%20Builder%20Using%20Streamlit"
+    "🏆 Digital Resume creation using Streamlit.":"https://github.com/Singhyash9009/GIT-HUB/tree/main/Resume%20Builder%20Using%20Streamlit"
 }
 
 st.set_page_config(page_title=PAGE_TITLE, page_icon=PAGE_ICON)
@@ -57,21 +60,26 @@ with col2:
     )
     st.write("📫", EMAIL)
 
-GitHub= Image.open(GitHub)
-LinkedIn=Image.open(LinkedIn)
-col1, col2, col3,col4,col5,col6 = st.columns([1,1,1,1,1,1])
-with st.container():
-    col1.image(LinkedIn, width=50)
-    col4.image(GitHub, width=50)
-# --- SOCIAL LINKS ---
+st.write("---")
+
 st.write('\n')
-cols = st.columns(len(SOCIAL_MEDIA))
-for index, (platform, link) in enumerate(SOCIAL_MEDIA.items()):
-    cols[index].write(f"[{platform}]({link})")
+st.subheader("Qualifications")
+st.write("""
+
+-      🎓 Advanced PGP in Data Science and Machine Learning 
+-   NIIT  Sep-2022 to Mar-2023
+-         🎓 Civil Engineering
+-   Shri Ramdeobaba College of Engineering & Management 2016-2020 
+
+-      🎓 Higher Secondary Certificate
+-   Macro Vision Academy 2015-2016
+
+"""
+)
 
 # --- EXPERIENCE & QUALIFICATIONS ---
 st.write('\n')
-st.subheader("Experience & Qualifications")
+st.subheader("Experience")
 st.write(
     """  
 - ✔️ Strong hands on experience and knowledge in Python,MySQL and Excel
@@ -87,7 +95,7 @@ st.subheader("Skills")
 st.write(
     """
 - 👩‍💻 Programming: Python (Scikit-learn, Pandas, Numpy), SQL
-- 📊 Data Visulization: PowerBi, MS Excel, Plotly
+- 📊 Data Visulization: Tableau,PowerBi, MS Excel, Plotly
 - 📚 Supervised Models: Logistic and Linear regression, Decision tree, Naive Bayes,Stacking Models
 - 📚 Unsupervised Models: K-Means Clustering , DBSCAN , Hierarchical Clustering
 - 🗄️ Databases: Postgres, MongoDB, MySQL
@@ -101,8 +109,8 @@ st.write("---")
 
 # --- JOB 1
 st.write("🚧", "**Data Science Intern | Bugendaitech**")
-st.write("05/2023 - 05/2023")
-st.write(
+st.write("05/2023 - 09/2023")
+st.markdown(
     """
 - ► Worked on Machine Learning Models created model for AirBnB Dataset
 - ► Used Tableau and PowerBI  to redeﬁne and track KPIs on sales of superstore and  recommendations to boost their sales in future
@@ -117,3 +125,23 @@ st.subheader("Projects & Accomplishments")
 st.write("---")
 for project, link in PROJECTS.items():
     st.write(f"[{project}]({link})")
+
+st.write('\n')
+st.write('---')
+st.write('\n')
+
+GitHub= Image.open(GitHub)
+LinkedIn=Image.open(LinkedIn)
+Gmail=Image.open(Gmail)
+col1, col2, col3,col4,col5,col6 = st.columns([1,1,1,1,1,1])
+with st.container():
+    col1.image(LinkedIn, width=50)
+    col3.image(GitHub, width=50)
+    col5.image(Gmail,width=50)
+# --- SOCIAL LINKS ---
+
+
+cols = st.columns(len(SOCIAL_MEDIA))
+for index, (platform, link) in enumerate(SOCIAL_MEDIA.items()):
+    cols[index].write(f"[{platform}]({link})")
+st.write('---')
